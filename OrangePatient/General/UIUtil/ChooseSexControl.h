52853aaf98582b1a4a6539f478b9a4f76d5db2e0
qing0602 <zq62@vip.qq.com>
2015-03-16 10:12:42 +0800

    add chooseSexControl

---
+++ OrangePatient/General/UIUtil/ChooseSexControl.h
@@ -0,0 +1,13 @@
+//
+//  ChooseSexControl.h
+//  OrangePatient
+//
+//  Created by ZhangQing on 3/16/15.
+//  Copyright (c) 2015 Orange. All rights reserved.
+//
+
+#import "BaseUIView.h"
+
+@interface ChooseSexControl : BaseUIView
+
+@end
