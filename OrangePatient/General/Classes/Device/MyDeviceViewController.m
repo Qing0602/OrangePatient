77e59c06b022ea0cd161e199ad9d6a5e66b36e13
wangshuo <9708022@qq.com>
2015-03-17 02:18:27 +0800

    asy

--- OrangePatient/General/Classes/Device/MyDeviceViewController.m
+++ OrangePatient/General/Classes/Device/MyDeviceViewController.m
@@ -16,21 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.translatesAutoresizingMaskIntoConstraints = NO;
     UIButton *searchButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [searchButton setImage:[UIImage imageNamed:@"SearchDevice"] forState:UIControlStateNormal];
     [searchButton setImage:[UIImage imageNamed:@"SearchDevice"] forState:UIControlStateHighlighted];
     searchButton.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:searchButton];
-    
+
     NSDictionary *views = NSDictionaryOfVariableBindings(searchButton);
     NSDictionary *metrics = @{@"imageEdge":@150.0};
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[searchButton(imageEdge)]" options:0 metrics:metrics views:views]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[searchButton(imageEdge)]" options:0 metrics:metrics views:views]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:searchButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:searchButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
-    
-    [self.view setBackgroundColor: [UIColor whiteColor]];
+
 }
 
 - (void)didReceiveMemoryWarning {
