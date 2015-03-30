4ee8496d835c813a3460c16e7285eb08c6a7e99f
qing0602 <zq62@vip.qq.com>
2015-03-30 12:43:58 +0800

    add files

---
+++ OrangePatient/General/Classes/Home/MyDoctor/MyDoctorListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyDoctorListViewController.h
+//  OrangePatient
+//
+//  Created by ZhangQing on 15/3/30.
+//  Copyright (c) 2015年 Orange. All rights reserved.
+//
+
+#import "OrangeBaseViewController.h"
+
+@interface MyDoctorListViewController : OrangeBaseViewController
+
+@end
