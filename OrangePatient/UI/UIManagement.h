a4d63f3a704d80a8b7184d84ed89a04242948d4e
wangshuo <9708022@qq.com>
2015-03-31 09:52:43 +0800

    asy

--- OrangePatient/UI/UIManagement.h
+++ OrangePatient/UI/UIManagement.h
@@ -21,5 +21,5 @@
 // 登陆
 -(void) login : (NSString *) userName withPassword : (NSString *) password;
 // 提供手机号码获取注册、重置等验证码。typeCode:0 - 注册, 1 - 重置密码
--(void) getVerifyCode:(NSString *)phoneNumber withType:(NSInteger)typeCode;
+-(void) getVerifyCode:(NSString *)phoneNumber withType:(NSInteger) typeCode;
 @end
