88448c8a984a835a10e72c6f1c1bc1b1cdf6da16
qing0602 <zq62@vip.qq.com>
2015-04-01 11:36:43 +0800

    add file

---
+++ OrangePatient/General/Classes/Home/MyDoctor/TwoTablesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwoTablesViewController.h
+//  OrangePatient
+//
+//  Created by ZhangQing on 15/4/1.
+//  Copyright (c) 2015年 Orange. All rights reserved.
+//
+
+#import "OrangeBaseViewController.h"
+
+@interface TwoTablesViewController : OrangeBaseViewController
+
+@end
