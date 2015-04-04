53ad13a92430ed398e0ddaaf6ed286e5954987d3
wangshuo <9708022@qq.com>
2015-04-05 07:49:41 +0800

    asy

---
+++ OrangePatient/General/Classes/Me/FeedBackViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedBackViewController.h
+//  OrangePatient
+//
+//  Created by singlew on 15/4/5.
+//  Copyright (c) 2015年 Orange. All rights reserved.
+//
+
+#import "OrangeBaseViewController.h"
+
+@interface FeedBackViewController : OrangeBaseViewController
+
+@end
