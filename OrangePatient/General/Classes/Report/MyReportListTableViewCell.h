a5b7c76f6fd53f02164d885d0a2691a73d487b15
qing0602 <zq62@vip.qq.com>
2015-04-20 13:31:50 +0800

    reportList

--- OrangePatient/General/Classes/Report/MyReportListTableViewCell.h
+++ OrangePatient/General/Classes/Report/MyReportListTableViewCell.h
@@ -5,9 +5,17 @@
 //  Created by ZhangQing on 15/4/19.
 //  Copyright (c) 2015年 Orange. All rights reserved.
 //
-
+#define MyReportListTableViewCell_Height 40
 #import <UIKit/UIKit.h>
+#import "MyReportListModel.h"
 
 @interface MyReportListTableViewCell : UITableViewCell
+//类型
+@property (nonatomic, strong)UILabel *typeLabel;
+//状态
+@property (nonatomic, strong)UILabel *statusLabel;
+//日期
+@property (nonatomic, strong)UILabel *dateLabel;
 
+- (void)setContentData:(MyReportListModel *)model;
 @end
--- OrangePatient/General/Classes/Report/MyReportListTableViewCell.m
+++ OrangePatient/General/Classes/Report/MyReportListTableViewCell.m
@@ -10,6 +10,60 @@
 
 @implementation MyReportListTableViewCell
 
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        UIImageView *imageIcon = [[UIImageView alloc] init];
+        imageIcon.backgroundColor = [UIColor redColor];
+        [self.contentView addSubview:imageIcon];
+        
+        _typeLabel = [[UILabel alloc] init];
+        _typeLabel.font = [UIFont systemFontOfSize:12.f];
+        [self.contentView addSubview:_typeLabel];
+        
+        _statusLabel = [[UILabel alloc] init];
+        _statusLabel.font = [UIFont systemFontOfSize:12.f];
+        _statusLabel.textAlignment = NSTextAlignmentCenter;
+        [self.contentView addSubview:_statusLabel];
+        
+        _dateLabel = [[UILabel alloc] init];
+        _dateLabel.font = [UIFont systemFontOfSize:12.f];
+        [self.contentView addSubview:_dateLabel];
+        
+        //_typeLabel.backgroundColor = _statusLabel.backgroundColor = _dateLabel.backgroundColor = [UIColor yellowColor];
+        
+        [imageIcon mas_makeConstraints:^(MASConstraintMaker *make){
+            make.top.mas_equalTo(10);
+            make.left.mas_equalTo(15);
+            make.width.and.height.mas_equalTo(MyReportListTableViewCell_Height-20);
+        }];
+        
+        [_typeLabel mas_makeConstraints:^(MASConstraintMaker *make){
+            make.top.equalTo(imageIcon.mas_top);
+            make.left.equalTo(imageIcon.mas_right).with.offset(10);
+            make.height.equalTo(imageIcon.mas_height);
+            make.width.mas_equalTo(112);
+        }];
+        
+        [_statusLabel mas_makeConstraints:^(MASConstraintMaker *make){
+            make.top.equalTo(imageIcon.mas_top);
+            make.left.equalTo(_typeLabel.mas_right).with.offset(10);
+            make.height.equalTo(imageIcon.mas_height);
+            make.width.mas_equalTo(60);
+        }];
+        
+        [_dateLabel mas_makeConstraints:^(MASConstraintMaker *make){
+            make.top.equalTo(imageIcon.mas_top);
+            make.left.equalTo(_statusLabel.mas_right).with.offset(10);
+            make.height.equalTo(imageIcon.mas_height);
+            make.right.mas_equalTo(-10);
+        }];
+        
+        
+    }
+    return self;
+}
+
 - (void)awakeFromNib {
     // Initialization code
 }
@@ -20,4 +74,9 @@
     // Configure the view for the selected state
 }
 
+- (void)setContentData:(MyReportListModel *)model{
+    self.typeLabel.text = model.reportType;
+    self.statusLabel.text = @"未读";
+    self.dateLabel.text = model.reportDate;
+}
 @end
