2a70c9fd1b17d40dd084d53e2119da4246897b91
qing0602 <zq62@vip.qq.com>
2015-04-20 16:36:40 +0800

    add file

---
+++ OrangePatient/General/Classes/Home/Information/HealthInfoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  HealthInfoDetailViewController.h
+//  OrangePatient
+//
+//  Created by ZhangQing on 15/4/20.
+//  Copyright (c) 2015年 Orange. All rights reserved.
+//
+
+#import "OrangeBaseViewController.h"
+
+@interface HealthInfoDetailViewController : OrangeBaseViewController
+
+@end
